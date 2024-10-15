fd15490e849ae24b967bd456f7a494ca76c1ab58
adithya <aditya93941@gmail.com>
2024-10-15 14:48:46 +0530

    .

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -7,14 +7,29 @@ class Projects extends Component {
                 <h1 className="Heading">Projects</h1>
                 <ul className="projectList">
                     <li className="projectCard">
+                        <img className="projectImg" src='https://ik.imagekit.io/ve7kfpijr/Screenshot%202024-10-15%20142844.png?updatedAt=1728982766476' alt="Game of Questions"/>
+                        <h2>Game of Questions</h2>
+                        <a className="projectLink" href="https://game-frontend-gamma.vercel.app/" target='_blank'>Click Here</a>
+                    </li>
+                    <li className="projectCard">
+                        <img className="projectImg" src='https://ik.imagekit.io/ve7kfpijr/Screenshot%202024-10-15%20144558.png' alt="Social Media Tasks"/>
+                        <h2>Social Media Tasks</h2>
+                        <a className="projectLink" href="https://social-media-tasks.vercel.app/" target='_blank'>Click Here</a>
+                    </li>
+                    <li className="projectCard">
                         <img className="projectImg" src='https://res.cloudinary.com/dm3fmkz3f/image/upload/v1724069184/Screenshot_2024-08-19_173536_ynbjcf.png' alt="Netflix clone"/>
-                        <h2>Netflix clone</h2>
+                        <h2>Netflix Clone</h2>
                         <a className="projectLink" href="https://adithyanetflix.ccbp.tech/" target='_blank'>Click Here</a>
                     </li>
                     <li className="projectCard">
+                        <img className="projectImg" src='https://ik.imagekit.io/ve7kfpijr/Screenshot%202024-10-15%20144201.png' alt="Fly Buy"/>
+                        <h2>Fly Buy</h2>
+                        <a className="projectLink" href="https://flybuyhub.ccbp.tech/" target='_blank'>Click Here</a>
+                    </li>
+                    <li className="projectCard">
                         <img className="projectImg" src='https://res.cloudinary.com/dm3fmkz3f/image/upload/v1724071984/Screenshot_2024-08-19_182242_drerx9.png' alt="FoodMunch"/>
-                        <h2>FoodMunch</h2>
-                        <a className="projectLink" href="https://adifoodmunch1.ccbp.tech/" target='_blank'>Click Here</a>
+                        <h2>Food Munch</h2>
+                        <a className="projectLink" href="https://adithyafoodweb.ccbp.tech/" target='_blank'>Click Here</a>
                     </li>
                     <li className="projectCard">
                         <img className="projectImg" src='https://ik.imagekit.io/ve7kfpijr/Screenshot%202024-08-20%20115300.png' alt="Bookmark Maker"/>
